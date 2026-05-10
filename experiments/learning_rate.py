f11771d7a0c98890e7c5efd5094fd297a9f2cbfa
Ilya Podmarkov <shmell.letitt12@gmail.com>
2026-05-10 18:08:10 +0300

    feat: add learning rate experiment

---
+++ experiments/learning_rate.py
@@ -0,0 +1,49 @@
+import matplotlib.pyplot as plt
+import sys
+from pathlib import Path
+
+sys.path.insert(0, str(Path(__file__).parent.parent))
+
+from src.perceptron import Perceptron
+from src.data_preprocessing import load_data
+from helpers.helpers import print_metrics
+
+EPOCHS = 100
+BATCH = 32
+
+X_train, y_train, X_val, y_val, X_test, y_test = load_data()  # загрузка данных
+
+fig, axes = plt.subplots(nrows=1, ncols=5, figsize=(15, 4))
+
+learning_rates = [0.001, 0.01, 0.5, 1.0]
+
+# graphics utils
+epoch_range = range(1, EPOCHS + 1)
+for axe, lr in enumerate(learning_rates):
+    perceptron = Perceptron()
+    
+    perceptron.fit(X_train, y_train, X_val, y_val, EPOCHS, lr, BATCH)  # обучение
+    
+    # loss graphic
+    axes[axe].plot(epoch_range, perceptron._train_losses, label="Train") 
+    axes[axe].plot(epoch_range, perceptron._val_losses, label="Val")   
+    axes[axe].set_xlabel('Epoch')
+    axes[axe].set_ylabel('Loss')
+    axes[axe].set_title(f'lr = {lr}')
+    axes[axe].legend()
+    axes[axe].grid(True, alpha=0.3)
+    
+    axes[len(learning_rates)].plot(epoch_range, perceptron._val_losses, label=f"lr={lr}")
+    
+    # metrics
+    metrics_test = perceptron.evaluate(X_test, y_test)
+    print_metrics(f"lr = {lr}", metrics_test)
+
+axes[len(learning_rates)].set_xlabel('Epoch')
+axes[len(learning_rates)].set_ylabel('Loss')
+axes[len(learning_rates)].set_title('Absolute compare')
+axes[len(learning_rates)].legend()
+axes[len(learning_rates)].grid(True, alpha=0.3)
+
+plt.tight_layout() 
+plt.show()
